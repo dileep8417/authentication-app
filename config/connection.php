3edd352192dcb125cecdb458e32c19cc52df90d3
Dileep <33929926+dileep8417@users.noreply.github.com>
2020-10-21 20:24:49 +0530

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -1,7 +1,7 @@
 <?php
-    $host = "remotemysql.com";
-    $user = "MueTiW6R4M";
-    $pwd = "lkb2CpSRFk";
-    $db = "MueTiW6R4M";
+    $host = "";
+    $user = "";
+    $pwd = "";
+    $db = "";
     $conn = mysqli_connect("$host","$user","$pwd","$db") or die("Database is not connected");
 ?>
